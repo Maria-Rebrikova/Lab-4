91e729a858d7efa6e228a673d5a5f9cdbba1167d
Maria Rebrikova <ardasheva.mm@phystech.edu>
2024-11-28 14:08:03 +0300

    Update waveMeasure.py

--- waveMeasure.py
+++ waveMeasure.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as gpio
 import sys
-from time import sleep
-import time
+from time import sleep, time
 from matplotlib import pyplot
+import os
 
 gpio.setmode(gpio.BCM)
 dac = [26, 19, 13, 6, 5, 11, 9, 10]
@@ -22,44 +22,55 @@ def perev(a):
 def adc():
     k = 0
     for i in range(7, -1, -1):
-        k += 2 ** i
+        k += 2 * i  # Исправлено: добавлено *
         gpio.output(dac, perev(k))
         sleep(0.005)
         if gpio.input(comp) == 0:
-            k -= 2 ** i
+            k -= 2 * i  # Исправлено: добавлено *
     return k
 
 
 def waitForOpen():
     print('GPIO initialized. Wait for door opening...')
-
     while gpio.input(knop_in) > 0:
-        pass
-
-    print('The door is open. GPIO has been cleaned up. Start sampling...')
+        sleep(0.1) # небольшая пауза, чтобы не перегружать процессор
+    print('The door is open. Start sampling...')
 
 
 volt = []
 time_start = 0
 time_len = 0
+sampling_duration = 10 # секунды
 
 try:
     waitForOpen()
     time_start = time.time()
-    while True:
+    end_time = time_start + sampling_duration
+    while time.time() < end_time:
         volt.append(adc() / 256 * 3.3)
+
+except KeyboardInterrupt:
+    print("Измерение прервано пользователем.")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
 
 
 finally:
     time_len = time.time() - time_start
-
-    with open('wave-data-120mm-kalibr.txt', 'w') as file:
+    filename = 'wave-data-120mm-kalibr.txt'
+    if os.path.exists(filename):
+        print(f"Внимание: файл '{filename}' уже существует. Данные будут перезаписаны.")
+    with open(filename, 'w') as file:
         file.write(str(time_len) + '\n')
         for i in volt:
             file.write(str(i) + '\n')
 
     time_step = [i * time_len / len(volt) for i in range(len(volt))]
     pyplot.plot(time_step, volt)
+    pyplot.xlabel("Время, с")
+    pyplot.ylabel("Напряжение, В")
+    pyplot.title("Зависимость напряжения от времени")
+    pyplot.grid(True)
     pyplot.show()
     gpio.output(dac, 0)
     gpio.cleanup()
